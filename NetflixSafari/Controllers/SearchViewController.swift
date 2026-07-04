cb184896bca968c40d3d39d8b017366a06df9824
sachin <sachin32jnv@gmail.com>
2026-07-04 16:59:23 +0530

    Bottom action tray tab bar added

---
+++ NetflixSafari/Controllers/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SerachViewController.swift
+//  NetflixSafari
+//
+//  Created by Sachin on 04/07/2026.
+//
+
+import UIKit
+
+class SerachViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+
+    }
+    
+
+   
+
+}

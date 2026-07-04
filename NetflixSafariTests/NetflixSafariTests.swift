bc6fc483839a8d8ee92aa3a9b3db8b985ea3e5c7
sachin <sachin32jnv@gmail.com>
2026-07-04 14:51:45 +0530

    Initial Commit

---
+++ NetflixSafariTests/NetflixSafariTests.swift
@@ -0,0 +1,19 @@
+//
+//  NetflixSafariTests.swift
+//  NetflixSafariTests
+//
+//  Created by Sachin on 04/07/2026.
+//
+
+import Testing
+@testable import NetflixSafari
+
+struct NetflixSafariTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}

7b7b5b5eca689ed7f08c0a15f91c5287d3627ec1
sachin <sachin32jnv@gmail.com>
2026-07-04 15:35:03 +0530

    Initial commit

---
+++ NetflixSafari/Controllers/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  NetflixSafari
+//
+//  Created by Sachin on 04/07/2026.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor  = .systemBlue
+        
+    }
+    
+
+
+
+}
---
+++ NetflixSafari/Controllers/UpComingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UpComingViewController.swift
+//  NetflixSafari
+//
+//  Created by Sachin on 04/07/2026.
+//
+
+import UIKit
+
+class UpComingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+}

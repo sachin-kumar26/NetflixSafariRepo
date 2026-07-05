28bc5935cdbfd1187da3787d98ff091eb3c4a7e7
sachin <sachin32jnv@gmail.com>
2026-07-05 17:00:12 +0530

    table header added and navBar items added on navbar

--- NetflixSafari/Controllers/HomeViewController.swift
+++ NetflixSafari/Controllers/HomeViewController.swift
@@ -17,22 +17,44 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor  = .systemMint
+        view.backgroundColor  = .systemBackground
         view.addSubview(homeFeedTable)
-        homeFeedTable.backgroundColor = .systemPink
-    
+        homeFeedTable.backgroundColor = .systemBackground
         homeFeedTable.delegate = self
         homeFeedTable.dataSource = self
-        homeFeedTable.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 400))
+        configureNavbar()
+        let headerView = HeroHeaderView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+        homeFeedTable.tableHeaderView = headerView
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         homeFeedTable.frame = view.bounds
         
-       
+        
+    }
+    
+    private func configureNavbar() {
+        var image = UIImage(named: "netflix_logo")
+        image = resizeImage(image, targetSize: CGSize(width: 30, height: 30))
+        image = image?.withRenderingMode(.alwaysOriginal)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: nil)
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(image: UIImage(systemName: "person"), style: .plain, target: self, action: nil),
+            UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .plain, target: self, action: nil)
+        ]
+        navigationController?.navigationBar.tintColor = .white
     }
 
+    /// this is to resixe image
+    private func resizeImage(_ image: UIImage?, targetSize: CGSize) -> UIImage? {
+        guard let image = image else { return nil }
+        let renderer = UIGraphicsImageRenderer(size: targetSize)
+        return renderer.image { _ in
+            image.draw(in: CGRect(origin: .zero, size: targetSize))
+        }
+    }
+    
 }
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
---
+++ NetflixSafari/Views/HeroHeaderView.swift
@@ -0,0 +1,85 @@
+//
+//  HeroHeaderView.swift
+//  NetflixSafari
+//
+//  Created by Sachin on 05/07/2026.
+//
+
+import UIKit
+
+class HeroHeaderView: UIView {
+    
+    private let downloadButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Download", for: .normal)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 8
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    private let playButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 8
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "hero_header")
+        return imageView
+    }()
+    
+    private func addGradient(){
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+        addGradient()
+        addSubview(playButton)
+        addSubview(downloadButton)
+        applyConstainsts()
+    }
+    
+    private func applyConstainsts(){
+        
+        let playBtnConstraints = [
+            playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 80),
+            playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            playButton.widthAnchor.constraint(equalToConstant: 110)
+        ]
+        let downloadBtnConstraints = [
+            downloadButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -80),
+            downloadButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            downloadButton.widthAnchor.constraint(equalToConstant: 110)
+        ]
+        NSLayoutConstraint.activate(playBtnConstraints)
+        NSLayoutConstraint.activate(downloadBtnConstraints)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
